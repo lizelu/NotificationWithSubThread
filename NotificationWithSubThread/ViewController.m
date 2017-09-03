d191f788a6c28743ebfbf9ba828acf500d78ca0f
ZeluLi <lizelusdut@qq.com>
2017-09-03 21:35:19 +0800

    update source code

--- NotificationWithSubThread/ViewController.m
+++ NotificationWithSubThread/ViewController.m
@@ -19,17 +19,29 @@
     [super viewDidLoad];
     NSLog(@"register notifcation thread = %@", [NSThread currentThread]);
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleNotification:) name:@"MyNAME" object:nil];
+    //往通知中心添加观察者
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(handleNotification:)
+                                                 name:@"MyNAME" object:nil];
     
+    //创建子线程，在子线程中发送通知
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSLog(@"post notification thread = %@", [NSThread currentThread]);
         [[NSNotificationCenter defaultCenter] postNotificationName:@"MyNAME" object:nil userInfo:nil];
     });
 }
 
+/**
+ 处理通知的方法
+
+ @param notification notification
+ */
 - (void)handleNotification:(NSNotification *)notification {
+    //打印处理通知的方法
     NSLog(@"handle notification thread = %@", [NSThread currentThread]);
 }
+
+
 
 - (IBAction)tapPushButton:(id)sender {
     [self.navigationController pushViewController:[SecondViewController new] animated:YES];
